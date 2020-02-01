4184a6616bfb0fa0e139dbe98eff889c83744290
beanjo55 <benschweitzer24@gmail.com>
2020-02-01 15:31:04 -0500

    check for null messages

--- commandHandler/CommandHandler.js
+++ commandHandler/CommandHandler.js
@@ -205,6 +205,9 @@ async function _prefixHandle(msg, Hyperion){
 
 //checks that would stop immediately 
 async function _preHandle(msg, Hyperion){
+    if(msg == null){
+        Hyperion.logger.warning("recieved null message")
+    }
     if(msg.member.bot){
         return false;
     }
