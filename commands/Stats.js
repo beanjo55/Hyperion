04f7b25d2b993131fd2378b9e4687af92decd33a
beanjo55 <benschweitzer24@gmail.com>
2020-02-01 16:22:58 -0500

    fixed decimal for stats

--- commands/Stats.js
+++ commands/Stats.js
@@ -15,6 +15,8 @@ class Stats extends command{
     }
 
     async execute(msg, args, Hyperion){
+        let usage = (process.memoryUsage().heapUsed/1024/1024)
+        usage = usage.toFixed(2);
         const embed = new Embed.DiscordEmbed()
         .setTitle("Hyperion stats")
         .setColor("#e87722")
@@ -22,8 +24,8 @@ class Stats extends command{
         .addField("Commands", `${Hyperion.commands.size} commands`, true)
         .addField("Guilds", `${Hyperion.guilds.size} Guilds`, true)
         .addField("Users", `${Hyperion.users.size} Users`, true)
-        .addField("CPU Usage", `${os.loadavg()[0]}`, true)
-        .addField("Memory Usage", `${(process.memoryUsage().heapUsed/1024/1024).toFixed(2)} mb`, true)
+        .addField("CPU Usage", `${os.loadavg()[0].toFixed(2)}%`, true)
+        .addField("Memory Usage", `${usage} mb`, true)
         .addField("Uptime", `${msc(Hyperion.uptime)}`, true);
         msg.channel.createMessage(embed);
     }
