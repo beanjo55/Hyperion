5565d6a99d212f4218609d2b268d6c5110f35e5c
ben <benschweitzer24@gmail.com>
2020-04-19 21:42:10 -0400

    fix build

--- src/Modules/Dev/Commands/Build.ts
+++ src/Modules/Dev/Commands/Build.ts
@@ -25,8 +25,15 @@ class Build extends Command{
         const commandString: string = "cd " + dir
         + " | curl https://circleci.com/api/v1.1/project/github/beanjo55/Hyperion/" + buildNum + "/artifacts?circle-token=" + Hyperion.circleCIToken
         + " | grep -o 'https://[^\"]*'"
-        + " | sed -e \"s/$/?circle-token=" + Hyperion.circleCIToken + "\""
-        + " | wget -v -i -";
+        + " | sed -e \"s/$/?circle-token=" + Hyperion.circleCIToken + "/\""
+        + " | wget -q --output-document=./built.tar -i -";
+
+        const stage2: string = "mv build/ oldBuild/"
+        + " | tar --overwrite -xf built.tar";
+        + " | rm -rf built.tar?circle-token=" + Hyperion.circleCIToken
+        + " | rm -rf built.tar"
+        + " | rm -rf oldBuild/";
+
         let message: string = "";
         if(buildNum === "latest"){
             message = "Downloading latest build from CircleCI";
@@ -37,8 +44,18 @@ class Build extends Command{
         try{
             // eslint-disable-next-line no-unused-vars
             exec(commandString, (error: any, stdout: any) =>{
-                if(error){return post.edit("Something went wrong");}
-                return post.edit(`Sucessfully downloaded and extarcted build ${buildNum}!`);
+                if(error){
+                    console.log(error);
+                    return post.edit("Download failed");
+                }
+                // eslint-disable-next-line no-unused-vars
+                exec(stage2, (error2: any, stdout2: any) => {
+                    if(error2){
+                        console.log(error2);
+                        return post.edit("Exctract failed");
+                    }
+                    return post.edit(`Sucessfully downloaded and extracted build ${buildNum}!`);
+                });
             });
         }catch(err){
             post.edit("Something went wrong");
