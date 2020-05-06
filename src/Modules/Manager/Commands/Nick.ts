3a47ef9bb1179b28f7335236cc23ea0634892ace
beanjo55 <benschweitzer24@gmail.com>
2020-05-06 14:50:02 -0400

    nick command

---
+++ src/Modules/Manager/Commands/Nick.ts
@@ -0,0 +1,41 @@
+import {Command} from "../../../Core/Structures/Command";
+// eslint-disable-next-line no-unused-vars
+import {CommandContext, HyperionInterface} from "../../../types";
+
+
+class Nick extends Command{
+    constructor(){
+        super({
+            name: "nick",
+            module: "manager",
+            aliases: ["nickname"],
+            userperms: ["manager"],
+            cooldownTime: 30000,
+
+            helpDetail: "Changes the bot nickname in the server",
+            helpUsage: "{prefix}nick [new nickname]",
+            helpUsageExample: "{prefix}nick Harbringer"
+        });
+    }
+
+    async execute(ctx: CommandContext, Hyperion: HyperionInterface){
+        let bot = ctx.guild.members.get(Hyperion.client.user.id);
+        if(!bot){return "A cache error occured";}
+        if(!bot.permission.has("changeNickname")){return "I dont have permissions to change my own nickname";}
+        if(!ctx.args[0]){
+            try{
+                await Hyperion.client.editNickname(ctx.guild.id, Hyperion.client.user.username, `User: ${ctx.user.username}#${ctx.user.discriminator}`);
+            }catch(err){
+                return "There was an error trying to change my nickname";
+            }
+            return "Successfully reset my nickname!";
+        }
+        try{
+            await Hyperion.client.editNickname(ctx.guild.id, ctx.args.join(), `User: ${ctx.user.username}#${ctx.user.discriminator}`);
+        }catch(err){
+            return "There was an error trying to change my nickname";
+        }
+        return "Success!";
+    }
+}
+export default Nick;
