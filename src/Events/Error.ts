3b38cde495676745f87d9fe86fb0610e446fca59
beanjo55 <benschweitzer24@gmail.com>
2020-05-12 12:19:16 -0400

    improve shard error logging

--- src/Events/Error.ts
+++ src/Events/Error.ts
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-vars */
 import {HyperionInterface} from "../types";
+import {inspect} from "util";
 class ErrorHandler{
     name: string;
     constructor(){
         this.name = "error";
     }
     async handle(this: HyperionInterface, err: Error, shardID: number): Promise<void>{
-        this.logger.error("Hyperion", "Error Event", `Shard ${shardID} encountered an error! Error: ${err}`);
+        this.logger.error("Hyperion", "Error Event", `Shard ${shardID} encountered an error! Error: ${inspect(err)}`);
+        this.sentry.captureException(err);
+
     }
 }
 exports.event = new ErrorHandler;
