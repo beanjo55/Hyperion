cb51e144b6bde3b302c74aec2a9dfa51c2ac0b8e
beanjo55 <benschweitzer24@gmail.com>
2020-06-01 14:19:44 -0400

    add support link command

---
+++ src/Modules/Info/Commands/Support.ts
@@ -0,0 +1,33 @@
+import {Command} from "../../../Core/Structures/Command";
+import {HyperionInterface, CommandContext} from "../../../types";
+import { Embed } from "eris";
+
+class Support extends Command{
+    constructor(){
+        super({
+            name: "support",
+            module: "info",
+
+            helpDetail: "Shows the support server invite, with the option to paste the raw invite in chat instead of embedding the link",
+            helpUsage: "{prefix}support\n{prefix}support raw",
+            noExample: true
+        });
+    }
+
+    async execute(ctx: CommandContext, Hyperion: HyperionInterface): Promise<string | {embed: Partial<Embed>}>{
+        if(ctx.args[0] && ctx.args[0].toLowerCase() === "raw"){
+            return "https://discord.gg/Vd8vmBD";
+        }else{
+            return {
+                embed: {
+                    color: Hyperion.defaultColor,
+                    timestamp: new Date,
+                    title: "Hyperion Support Server",
+                    description: "[Join the support server here!](https://discord.gg/Vd8vmBD)"
+                }
+            };
+        }
+    }
+}
+
+export default Support;
