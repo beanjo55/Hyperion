f1e084bd95701c9cc8c921e9a8db5e31403c8627
beanjo55 <benschweitzer24@gmail.com>
2020-06-21 17:44:41 -0400

    add kick and softban

---
+++ src/Modules/Mod/Commands/Kick.ts
@@ -0,0 +1,62 @@
+import {Command} from "../../../Core/Structures/Command";
+import { ICommandContext, IHyperion } from "../../../types";
+import { User } from "eris";
+
+
+
+class Kick extends Command{
+    constructor(){
+        super({
+            name: "kick",
+            userperms: ["mod"],
+            module: "mod",
+            needsRolepos: true,
+            cooldownTime: 5000,
+            helpDetail: "Kicks a user",
+            helpUsage: "{prefix}kick [user] [reason]",
+            helpUsageExample: "{prefix}kick boss trolling"
+        });
+    }
+
+    async execute(ctx: ICommandContext, Hyperion: IHyperion): Promise<string>{
+        const bot = ctx.guild.members.get(Hyperion.client.user.id);
+        if(!bot){return "Cache Failure, couldnt find bot user";}
+        if(!bot.permission.has("kickMembers")){return " I need the `Kick Members` permission to well, kick members";}
+        const toKick = await Hyperion.utils.strictResolver(ctx.args[0], ctx.guild.members);
+        if(!toKick){return "I couldnt figure out what user that is";}
+        if(toKick.id === ctx.user.id){return "You cant kick yourself";}
+        if(toKick.id === Hyperion.client.user.id){return "I cant kick myself";}
+        if(await ctx.module.isMod(Hyperion, toKick, ctx.guild)){return "That user is a mod and is protected from mod actions";}
+        if(bot.roles.length === 0){return "I need a role higehr than the user's highest role to kick them, I cant do that with no roles";}
+        const userRoles = Hyperion.utils.sortRoles(toKick.roles, ctx.guild.roles);
+        const botRoles = Hyperion.utils.sortRoles(bot.roles, ctx.guild.roles);
+        if(userRoles[0] && userRoles[0].position >= botRoles[0].position){return "I cant kick someone with the same highest role or a higher role than me";}
+        return await this.doKick(ctx, Hyperion,  toKick.user, ctx.args.slice(1).join(" "));
+    }
+
+    async doKick(ctx: ICommandContext, Hyperion: IHyperion, user: User, reason: string): Promise<string>{
+        if(!reason){reason = "No reason given";}
+        let auditReason = reason;
+        if(auditReason.length > 509){
+            auditReason = auditReason.substring(0, 508) + "...";
+        }
+        try{
+            await ctx.guild.kickMember(user.id, auditReason);
+        }catch{
+            return "Something went wrong!";
+        }
+        ctx.module.makeLog(Hyperion, {
+            user: user.id,
+            moderator: ctx.member.id,
+            moderationType: "kick",
+            reason: reason,
+            auto: false,
+            case: -1,
+            guild: ctx.guild,
+            time: Date.now(),
+            moderationEnd: false
+        }, user);
+        return `Kicked ${user.username}#${user.discriminator}`;
+    }
+}
+export default Kick;
---
+++ src/Modules/Mod/Commands/Softban.ts
@@ -0,0 +1,65 @@
+import {Command} from "../../../Core/Structures/Command";
+import { ICommandContext, IHyperion } from "../../../types";
+import { User } from "eris";
+
+
+
+class Softban extends Command{
+    constructor(){
+        super({
+            name: "softban",
+            userperms: ["mod"],
+            aliases: ["hardkick"],
+            module: "mod",
+            needsRolepos: true,
+            cooldownTime: 5000,
+            helpDetail: "Bans then unbans a user to clear their recent messages",
+            helpUsage: "{prefix}softban [user] [reason]",
+            helpUsageExample: "{prefix}softban wuper trolling"
+        });
+    }
+
+    async execute(ctx: ICommandContext, Hyperion: IHyperion): Promise<string>{
+        const bot = ctx.guild.members.get(Hyperion.client.user.id);
+        if(!bot){return "Cache Failure, couldnt find bot user";}
+        if(!bot.permission.has("banMembers")){return " I need the ban members permission to well, ban members";}
+        const toBan = await Hyperion.utils.strictResolver(ctx.args[0], ctx.guild.members);
+        if(!toBan){return "I couldnt figure out what user that is";}
+        if(toBan.id === ctx.user.id){return "You cant softban yourself";}
+        if(toBan.id === Hyperion.client.user.id){return "I cant softban myself";}
+        if(await ctx.module.isMod(Hyperion, toBan, ctx.guild)){return "That user is a mod and is protected from mod actions";}
+        if(bot.roles.length === 0){return "I need a role higehr than the user's highest role to softban them, I cant do that with no roles";}
+        const userRoles = Hyperion.utils.sortRoles(toBan.roles, ctx.guild.roles);
+        const botRoles = Hyperion.utils.sortRoles(bot.roles, ctx.guild.roles);
+        if(userRoles[0] && userRoles[0].position >= botRoles[0].position){return "I cant softban someone with the same highest role or a higher role than me";}
+        return await this.doBan(ctx, Hyperion,  toBan.user, ctx.args.slice(1).join(" "));
+        
+    }
+
+    async doBan(ctx: ICommandContext, Hyperion: IHyperion, user: User, reason: string): Promise<string>{
+        if(!reason){reason = "No reason given";}
+        let auditReason = reason;
+        if(auditReason.length > 509){
+            auditReason = auditReason.substring(0, 508) + "...";
+        }
+        try{
+            await ctx.guild.banMember(user.id, 7, auditReason);
+            await ctx.guild.unbanMember(user.id);
+        }catch{
+            return "Something went wrong!";
+        }
+        ctx.module.makeLog(Hyperion, {
+            user: user.id,
+            moderator: ctx.member.id,
+            moderationType: "softban",
+            reason: reason,
+            auto: false,
+            case: -1,
+            guild: ctx.guild,
+            time: Date.now(),
+            moderationEnd: false
+        }, user);
+        return `Softbanned ${user.username}#${user.discriminator}`;
+    }
+}
+export default Softban;
