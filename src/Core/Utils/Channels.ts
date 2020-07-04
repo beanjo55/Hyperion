f09a3313a13a4c9c4f2cfef0afbe2de82b2044da
beanjo55 <benschweitzer24@gmail.com>
2020-07-04 00:21:29 -0400

    add guild channel resolver

--- src/Core/Utils/Channels.ts
+++ src/Core/Utils/Channels.ts
@@ -38,3 +38,14 @@ export function resolveCategory(guild: Guild, msg: Message,  search: string): Ca
     if(channel && channel.type !== 4){return;}
     return channel;
 }
+
+export function resolveGuildChannel(guild: Guild, msg: Message, search: string): GuildChannel | undefined{
+    let channel = guild.channels.get(search);
+    if(!channel && msg?.channelMentions && msg.channelMentions[0]){
+        channel = guild.channels.get(msg.channelMentions[0]);
+    }
+    if(!channel){
+        channel = guild.channels.find((C: GuildChannel) => C.name.toLowerCase().startsWith(search.toLowerCase()));
+    }
+    return channel;
+}
