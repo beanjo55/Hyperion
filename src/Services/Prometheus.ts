477072fa4eeff32ec80ffe302c45a5a646e4b581
beanjo55 <benschweitzer24@gmail.com>
2020-07-23 16:53:17 -0400

    add more stat collection

--- src/Services/Prometheus.ts
+++ src/Services/Prometheus.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {default as express} from "express";
 import {register, AggregatorRegistry, default as client} from "prom-client";
 import {BaseServiceWorker, Setup} from "../Core/Services/BaseServiceWorker";
@@ -48,6 +49,58 @@ export default class Prometheus extends BaseServiceWorker{
             async collect(): Promise<void>{
                 const stats = await ipc.getStats();
                 this.set(stats.users);
+            }
+        });
+        const exclusive = new client.Gauge({
+            name: "Exclusive_Guilds",
+            help: "Exclusive Guilds",
+            async collect(): Promise<void>{
+                const stats = await ipc.getStats();
+                this.set(stats.exclusiveGuilds);
+            }
+        });
+        const unavailable = new client.Gauge({
+            name: "Unavailable_Guilds",
+            help: "Unavailable Guilds",
+            async collect(): Promise<void>{
+                const stats = await ipc.getStats();
+                this.set(stats.unavailableGuilds);
+            }
+        });
+        const clusterram = new client.Gauge({
+            name: "Cluster_Ram",
+            help: "Cluster Ram",
+            async collect(): Promise<void>{
+                const stats = await ipc.getStats();
+                this.set(stats.clustersRam);
+            }
+        });
+        const serviceram = new client.Gauge({
+            name: "Service_Ram",
+            help: "Service Ram",
+            async collect(): Promise<void>{
+                const stats = await ipc.getStats();
+                this.set(stats.servicesRam);
+            }
+        });
+        const masterram = new client.Gauge({
+            name: "Master_Ram",
+            help: "Master Ram",
+            async collect(): Promise<void>{
+                const stats = await ipc.getStats();
+                this.set(stats.masterRam);
+            }
+        });
+        const averageuptime = new client.Gauge({
+            name: "Average_Uptime",
+            help: "Average Uptime",
+            async collect(): Promise<void>{
+                const stats = await ipc.getStats();
+                let sum = 0;
+                for(const clust of stats.clusters){
+                    sum += clust.uptime;
+                }
+                this.set(sum/stats.clusters.length);
             }
         });
         server.listen(9990);
