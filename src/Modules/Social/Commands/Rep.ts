4ccb1da1b2df77bb4fd50fee99dc7939bf5e7052
beanjo55 <benschweitzer24@gmail.com>
2020-10-05 17:39:38 -0400

    allowed mentions for rep

--- src/Modules/Social/Commands/Rep.ts
+++ src/Modules/Social/Commands/Rep.ts
@@ -14,25 +14,26 @@ class Rep extends Command{
         });
     }
 
-    async execute(ctx: ICommandContext, Hyperion: IHyperion): Promise<string>{
+    // eslint-disable-next-line complexity
+    async execute(ctx: ICommandContext, Hyperion: IHyperion): Promise<{content: string, allowedMentions?: {users: Array<string>}}>{
         if(ctx.args[0] && ctx.args[1] && (ctx.args[0].toLowerCase() === "void" || (ctx.args[0].toLowerCase() === "the" && ctx.args[1].toLowerCase() === "void"))){
             const time = await Hyperion.managers.user.getRepTime(ctx.user.id);
             const acks = await Hyperion.managers.user.getAcks(ctx.user.id);
             const period = acks.pro ? day/2 : day;
             if(ctx.args.length === 0){
                 if(Date.now() - time <= period){
-                    return `You can give more rep in ${msc(period - (Date.now()-time))}`;
+                    return {content: `You can give more rep in ${msc(period - (Date.now()-time))}`};
                 }else{
-                    return "You can give a rep point now!";
+                    return {content: "You can give a rep point now!"};
                 }
             }
             if((Date.now() - time <= period) && !ctx.admin){
-                return `You can give more rep in ${msc(period - (Date.now()-time))}`;
+                return {content: `You can give more rep in ${msc(period - (Date.now()-time))}`};
             }
             Hyperion.redis.incr("void");
             const voidCount = await Hyperion.redis.get("void");
             Hyperion.managers.user.setRepTime(ctx.user.id);
-            return `You threw your rep to the void. ${voidCount} reps have been discarded there so far`;
+            return {content: `You threw your rep to the void. ${voidCount} reps have been discarded there so far`};
         }
         const lucky = randomInt(0, 100) === 69;
         const target = await Hyperion.utils.op8(ctx.args[0], ctx.guild);
@@ -41,16 +42,16 @@ class Rep extends Command{
         const period = acks.pro ? day/2 : day;
         if(ctx.args.length === 0){
             if(Date.now() - time <= period){
-                return `You can give more rep in ${msc(period - (Date.now()-time))}`;
+                return {content: `You can give more rep in ${msc(period - (Date.now()-time))}`};
             }else{
-                return "You can give a rep point now!";
+                return {content: "You can give a rep point now!"};
             }
         }
         if((Date.now() - time <= period) && !ctx.admin){
-            return `You can give more rep in ${msc(period - (Date.now()-time))}`;
+            return {content: `You can give more rep in ${msc(period - (Date.now()-time))}`};
         }
-        if(!target){return "A valid user was not found!";}
-        if(target.id === ctx.user.id){return "You can't rep yourself!";}
+        if(!target){return {content: "A valid user was not found!"};}
+        if(target.id === ctx.user.id){return {content: "You can't rep yourself!"};}
         Hyperion.managers.user.gotRep(target.id);
         Hyperion.managers.user.gaveRep(ctx.user.id);
         Hyperion.managers.user.setRepTime(ctx.user.id);
@@ -59,7 +60,13 @@ class Rep extends Command{
             Hyperion.managers.user.gotRep(ctx.user.id);
             out += "\nLooks like it's your lucky day! You have also gained a rep point! <a:happykitty:734450859026546699>";
         }
-        return out;
+        const allowedMentions: {users: Array<string>} = {users: []};
+        if(ctx.msg.mentions[0] && ctx.msg.mentions[0].id !== target.id){
+            allowedMentions.users.push(target.id);
+        }else{
+            allowedMentions.users.push(target.id);
+        }
+        return {content: out, allowedMentions};
     }
 }
 export default Rep;
