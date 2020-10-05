4486a99ae6a71dd0ef92215d18f0f6e0f14d9d19
beanjo55 <benschweitzer24@gmail.com>
2020-10-05 17:43:34 -0400

    reaction roles, 2.6.0

--- src/Modules/Reactionroles/Commands/Reactionroles.ts
+++ src/Modules/Reactionroles/Commands/Reactionroles.ts
@@ -1,6 +1,10 @@
 import {Command} from "../../../Core/Structures/Command";
-import {IHyperion, ICommandContext} from "../../../types";
+import {IHyperion, ICommandContext, CommandResponse} from "../../../types";
 import {ReactionRole, RRConfig} from "../../../Core/Managers/MongoGuildManager";
+import {default as rrM} from "../Reactionroles";
+import HyperionC from "../../../main";
+import { convertCompilerOptionsFromJson } from "typescript";
+import { GuildTextableChannel, Message } from "eris";
 
 class Reactionroles extends Command{
     constructor(){
@@ -10,36 +14,313 @@ class Reactionroles extends Command{
             listUnder: "manager",
             module: "reactionroles",
             userperms: ["manager"],
-            helpDetail: "Manages reaction roles"
+            helpDetail: "Manages reaction roles",
+            hasSub: true
         });
     }
 
-    async execute(ctx: ICommandContext, Hyperion: IHyperion): Promise<string>{
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
         const EmoteRegex = new RegExp(/<(a)?:(\w+):(\d+)>/, "gmi");
-        const channel = Hyperion.utils.resolveTextChannel(ctx.guild, ctx.msg, ctx.args[0]);
-        if(!channel){return "no channel";}
-        const message = channel.messages.get(ctx.args[1]) ?? await channel.getMessage(ctx.args[1]);
-        if(!message){return "no message";}
-        const emote = EmoteRegex.exec(ctx.args[2]);
-        if(!emote){return "no emote";}
-        const emoteName = emote![2];
-        const emoteID = emote?.[3];
-        const role = Hyperion.utils.resolveRole(ctx.args[3], ctx.guild.roles);
-        if(!role){return "no role";}
-        const name = ctx.args[4];
-        const erMap = new Map<string, string>();
-        erMap.set(emoteName, role.id);
-        const rr = new ReactionRole({
-            
-            erMap,
-            channel: channel.id
+        return await this.list(ctx, Hyperion);
+    }
+
+    async list(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const data = await ctx.module.getConfig(ctx.guild.id);
+        let out = "";
+        data.rr.forEach((rr, name) => {
+            out += `${name} - ${rr.erMap.size ?? 0} Roles\n`;
         });
-        const config = await Hyperion.managers.guild.getModuleConfig<RRConfig>(ctx.guild.id, "reactionroles");
-        if(!(config.rr instanceof Map)){config.rr = new Map<string, ReactionRole>(Object.entries(config.rr ?? {}));}
-        config.rr.set(message.id, rr);
-        await Hyperion.managers.guild.updateModuleConfig(ctx.guild.id, "reactionroles", config);
-        await message.addReaction(emoteID !== undefined ? `${emoteName}:${emoteID}` : emoteName);
-        return "added reaction role";
+        if(out === ""){out = "No Reaction Roles setup";}
+        return {
+            embed: {
+                color: Hyperion.colors.blue,
+                timestamp: new Date,
+                title: "Reaction Roles",
+                description: out
+            }
+        };
+    }
+
+    isEmote(input: string, Hyperion: IHyperion): boolean {
+        const EmoteRegex = new RegExp(/<(a)?:(\w+):(\d+)>/, "gmi");
+        const result = EmoteRegex.exec(input);
+        if(result){return true;}
+        return Hyperion.utils.hasUnicodeEmote(input);
+    }
+
+    parseEmote(input: string, Hyperion: IHyperion): {name: string; id?: string, full: string} | undefined {
+        const EmoteRegex = new RegExp(/<(a)?:(\w+):(\d+)>/, "gmi");
+        const result = EmoteRegex.exec(input);
+        if(result){
+            return {name: result[2], id: result[3], full: input};
+        }
+        if(Hyperion.utils.hasUnicodeEmote(input)){return {name: input, full: input};}
     }
 }
+
+class List extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "list";
+        this.aliases = [];
+        this.id = this.name;
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        return await this.list(ctx, Hyperion);
+    }
+}
+
+class Create extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "create";
+        this.aliases = [];
+        this.id = this.name;
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const name = ctx.args[1];
+        if(!name){return {status: "error", response: "Please give a name for the new reaction role"};}
+        try{
+            await ctx.module.createRR(ctx.guild.id, name);
+            return {status: "success", response: "Added reaction role " + name};
+        }catch(err){
+            return {status: "error", response: err.message};
+        }
+    }
+}
+
+class Delete extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "delete";
+        this.aliases = [];
+        this.id = this.name;
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const name = ctx.args[1];
+        if(!name){return {status: "error", response: "Please give a name for the reaction role to delete"};}
+        try{
+            await ctx.module.deleteRR(ctx.guild.id, name);
+            return {status: "success", response: "Removed reaction role " + name};
+        }catch(err){
+            return {status: "error", response: err.message};
+        }
+    }
+}
+
+class Add extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "add";
+        this.aliases = [];
+        this.id = this.name;
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const name = ctx.args[1];
+        if(!name){return {status: "error", response: "Please give a name for the reaction role to add to"};}
+        const emote = ctx.args[2];
+        if(!ctx.module.isEmote(emote)){return {status: "error", response: "Please give a valid emote"};}
+        const role = ctx.args[3];
+        const realRole = Hyperion.utils.resolveRole(role, ctx.guild.roles);
+        if(!realRole){return {status: "error", response: "Please specify a valid role"};}
+        try{
+            await ctx.module.addRole(ctx.guild.id, name, emote, realRole.id);
+            return {status: "success", response: "Added the emote/role pair to " + name};
+        }catch(err){
+            return {status: "error", response: err.message};
+        }
+    }
+}
+
+class Remove extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "remove";
+        this.aliases = [];
+        this.id = this.name;
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const name = ctx.args[1];
+        if(!name){return {status: "error", response: "Please give a name for the reaction role to remove from"};}
+        const emote = ctx.args[2];
+        try{
+            await ctx.module.delRole(ctx.guild.id, name, emote);
+            return {status: "success", response: "Removed the emote/role pair from " + name};
+        }catch(err){
+            return {status: "error", response: err.message};
+        }
+    }
+}
+
+class Show extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "show";
+        this.aliases = [];
+        this.id = this.name;
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const name = ctx.args[1];
+        if(!name){return {status: "error", response: "Please give a reaction role to show"};}
+        const config = await ctx.module.getConfig(ctx.guild.id);
+        const rr = config.rr.get(name);
+        if(!rr){return {status: "error", response: "Coundnt find a reaction role by that name in the server"};}
+        let string = "";
+        if(rr.erMap.size === 0){
+            string = "This reaction role has no emote/role pairs added";
+        }else{
+            rr.erMap.forEach((role, emote) => string += `${emote} - ${ctx.guild.roles.get(role)?.mention ?? "Deleted Role"}\n`);
+        }
+
+        const data = {
+            embed: {
+                title: `Reaction Role: ${name}`,
+                description: "__Emote - Role pairs__\n" + string,
+                color: Hyperion.colors.blue,
+                timestamp: new Date
+            }
+        };
+        return data;
+    }
+}
+
+class Post extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "post";
+        this.id = this.name;
+        this.aliases = [];
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const name = ctx.args[1];
+        const channel = ctx.args[2];
+        if(!name){return {status: "error", response: "Please specify a reaction role to post"};}
+        if(!channel){return {status: "error", response: "Please specify a channel to post the reaction role in"};}
+        const channelObj = Hyperion.utils.resolveTextChannel(ctx.guild, ctx.msg, channel);
+        if(!channelObj){return {status: "error", response: "Couldnt find that channel"};}
+        const config = await ctx.module.getConfig(ctx.guild.id);
+        const rr = config.rr.get(name);
+        if(!rr){return {status: "error", response: "Couldnt find that reaction role"};}
+        if(rr.linkedMessages.length > 0){return {status: "neutral", response: "That reaction role has already been posted"};}
+        const title = ctx.args.length >= 3 ? ctx.args.slice(3).join(" ") : "Reaction Role";
+        const names: Array<string> = [];
+        rr.erMap.forEach((role, emote) => {
+            const rle = ctx.guild.roles.get(role);
+            names.push(`${emote} - ${rle?.mention ?? "Unknown Role"}`);
+        });
+        const data = {
+            embed: {
+                description: `React to the emotes to get these roles:\n${names.join(", ")}`,
+                title,
+                timestamp: new Date,
+                color: Hyperion.colors.default
+            }
+        };
+        if(!ctx.module.checkAddPerms(channelObj)){return {status: "error", response: "I dont have permissions to read message history and/or add reactions in that channel"};}
+        try{
+            const message = await channelObj.createMessage(data);
+            const result = await ctx.module.addReactions([...rr.erMap.keys()], message);
+            if(result.failed.length === 0){
+                config.rrMessages.set(message.id, name);
+                rr.linkedMessages.push(message.id);
+                config.rr.set(name, rr);
+                await Hyperion.managers.guild.updateModuleConfig(ctx.guild.id, "reactionroles", config);
+                return {status: "fancySuccess", response: "Successfully posted reaction role!"};
+            }else{
+                message.delete().catch(() => undefined);
+                return {status: "error", response: `Couldnt add reaction ${result.failed[0]}, for the reason: ${result.reason}`};
+            }
+        }catch(err){
+            return {status: "error", response: "Something went wrong, " + err.message};
+        }
+    }
+}
+
+class Reset extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "reset";
+        this.aliases = [];
+        this.id = this.name;
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const name = ctx.args[1];
+        if(!name){return {status: "error", response: "Please give a name for the reaction role to reset linked messages for"};}
+        try{
+            const config = await ctx.module.getConfig(ctx.guild.id);
+            if(!config.rr.has(name)){return {status: "neutral", response: "I couldnt find that reaction role in this server"};}
+            const rr = config.rr.get(name);
+            rr!.linkedMessages = [];
+            config.rr.set(name, rr!);
+            config.rrMessages.forEach((key, msg) => {if(key === name){config.rrMessages.delete(msg);}});
+            await Hyperion.managers.guild.updateModuleConfig(ctx.guild.id, "reactionroles", config);
+            return {status: "success", response: "Reset linked messages for " + name};
+        }catch(err){
+            return {status: "error", response: err.message};
+        }
+    }
+}
+
+
+class Attach extends Reactionroles{
+    constructor(){
+        super();
+        this.name = "attach";
+        this.aliases = [];
+        this.id = this.name;
+    }
+
+    async execute(ctx: ICommandContext<rrM>, Hyperion: IHyperion): CommandResponse{
+        const name = ctx.args[1];
+        if(!name){return {status: "error", response: "Please give a name for the reaction role to attach"};}
+        const config = await ctx.module.getConfig(ctx.guild.id);
+        const rr = config.rr.get(name);
+        if(!rr){return {status: "error", response: "Couldnt find that reaction role"};}
+        if(rr.linkedMessages.length > 0){return {status: "neutral", response: "That reaction role has already been posted"};}
+        const channelOrLink = ctx.args[2];
+        const linkTest = Hyperion.utils.parseMessageLink(channelOrLink);
+        let channel, message: undefined | Message;
+        if(linkTest){
+            if(linkTest.guild !== ctx.guild.id){return {status: "error", response: "That link isnt even for this server!"};}
+            channel = ctx.guild.channels.get(linkTest.channel);
+            if(!channel){return {status: "neutral", response: "I couldnt find that channel in the server"};}
+            if(!(channel.type === 0 || channel.type === 5)){return {status: "error", response: "Please link me to a text or announcement channel"};}
+            message = channel.messages.get(linkTest.message) ?? await channel.getMessage(linkTest.message).catch(() => undefined);
+            if(!message){return {status: "neutral", response: "I couldnt find that message in that channel"};}
+        }else{
+            const temp = Hyperion.utils.resolveTextChannel(ctx.guild, ctx.msg, channelOrLink);
+            if(!temp){return {status: "neutral", response: "Could not find that channel in the server"};}
+            channel = temp;
+            message = channel.messages.get(ctx.args[3]) ?? await channel.getMessage(ctx.args[3]).catch(() => undefined);
+            if(!message){return {status: "neutral", response: "Could not find that message in that channel"};}
+        }
+        if(!ctx.module.checkAddPerms(channel)){return {status: "error", response: "I dont have permissions to read message history and/or add reactions in that channel"};}
+        try{
+            const result = await ctx.module.addReactions([...rr.erMap.keys()], message);
+            if(result.failed.length === 0){
+                config.rrMessages.set(message.id, name);
+                rr.linkedMessages.push(message.id);
+                config.rr.set(name, rr);
+                await Hyperion.managers.guild.updateModuleConfig(ctx.guild.id, "reactionroles", config);
+                return {status: "fancySuccess", response: "Successfully posted reaction role!"};
+            }else{
+                return {status: "error", response: `Couldnt add reaction ${result.failed[0]}, for the reason: ${result.reason}`};
+            }
+        }catch(err){
+            return {status: "error", response: "Something went wrong, " + err.message};
+        }
+    }
+}
+
+
+const subarr = [List, Create, Delete, Add, Remove, Show, Post, Reset, Attach];
 export default Reactionroles;
+export {subarr as subcmd};
--- src/Modules/Reactionroles/Reactionroles.ts
+++ src/Modules/Reactionroles/Reactionroles.ts
@@ -1,6 +1,6 @@
 import { Emoji, GuildTextableChannel, Message } from "eris";
 import {Module} from "../../Core/Structures/Module";
-import { IHyperion } from "../../types";
+import { EmbedResponse, IHyperion } from "../../types";
 import {RRConfig, ReactionRole} from "../../Core/Managers/MongoGuildManager";
 
 class Reactionroles extends Module{
@@ -14,59 +14,83 @@ class Reactionroles extends Module{
             needsLoad: false,
             dirname: __dirname,
             hasCommands: true,
-            subscribedEvents: ["messageReactionAdd", "messageReactionRemove"]
+            defaultStatus: true,
+            subscribedEvents: ["messageReactionAdd", "messageReactionRemove", "messsageDelete"]
         }, Hyperion);
     }
 
-    async messageReactionAdd(Hyperion: IHyperion, msg: Message, emote: Emoji, user: string): Promise<void>{
-        if(!msg.author){msg = await msg.channel.getMessage(msg.id);}
-        const channel = msg.channel;
-        if(!(msg.channel.type === 5 || msg.channel.type === 0)){return;}
+    async messageDelete(Hyperion: IHyperion, msg: Message | {id: string; channel: GuildTextableChannel}): Promise<void>{
+        if(!(msg.channel.type === 0 || msg.channel.type === 5)){return;}
         const guild = msg.channel.guild;
-        if(!await this.checkGuildEnabled(guild.id)){return;}
-        const config = await this.Hyperion.managers.guild.getModuleConfig<RRConfig>(guild.id, this.name);
-        if(!(config.rr instanceof Map)){config.rr = new Map<string, ReactionRole>(Object.entries(config.rr ?? {}));}
-        if(config.rr.size === 0){return;}
-        const rrName = config.rrMessages.get(msg.id);
-        if(!rrName){return;}
-        const rr = config.rr.get(rrName);
+        const enabled = await this.checkGuildEnabled(guild.id);
+        if(!enabled){return;}
+        const config = await this.getConfig(guild.id);
+        if(config.rrMessages.size === 0){return;}
+        const name = config.rrMessages.get(msg.id);
+        if(!name){return;}
+        config.rrMessages.delete(msg.id);
+        const rr = config.rr.get(name);
         if(!rr){return;}
-        if(!(rr.erMap instanceof Map)){rr.erMap = new Map<string, string>(Object.entries(rr.erMap ?? {}));}
-        const roleID = rr.erMap.get(emote.name);
-        if(!roleID){return;}
-        const role = guild.roles.get(roleID);
+        rr.linkedMessages = [];
+        config.rr.set(name, rr);
+        await Hyperion.managers.guild.updateModuleConfig(guild.id, this.name, config);
+    }
+
+    async messageReactionAdd(Hyperion: IHyperion, msg: Message, emote: Emoji, user: string): Promise<void>{
+        if(!(msg.channel.type === 0 || msg.channel.type === 5)){return;}
+        const guild = msg.channel.guild;
+        const enabled = await this.checkGuildEnabled(guild.id);
+        if(!enabled){return;}
+        const config = await this.getConfig(guild.id);
+        if(config.rrMessages.size === 0){return;}
+        const rr = config.rrMessages.get(msg.id);
+        if(!rr){return;}
+        const rrData = config.rr.get(rr);
+        if(!rrData){return;}
+        const result = this.matchEmote([...rrData.erMap.keys()], emote);
+        if(!result){return;}
+        const role = rrData.erMap.get(result);
         if(!role){return;}
-        const member = guild.members.get(user) ?? await guild.getRESTMember(user).catch(() => undefined);
-        if(!member){return;}
-        if(member.bot){return;}
-        member.addRole(roleID, "Hyperion Reaction Role").catch(err => this.Hyperion.logger.error("Hyperion", `Failed to add reaction role, err: ${err}`, "Reaction Roles"));
+        const usr = this.Hyperion.client.users.get(user) || await this.Hyperion.client.getRESTUser(user).catch(() => undefined);
+        if(usr && usr.bot){return;}
+        guild.addMemberRole(user, role, "Hyperion Reaction Role").catch(err => console.warn(`Failed to give RR: ${err.message}`));
     }
 
     async messageReactionRemove(Hyperion: IHyperion, msg: Message, emote: Emoji, user: string): Promise<void>{
-        if(!msg.author){msg = await msg.channel.getMessage(msg.id);}
-        const channel = msg.channel;
-        if(!(msg.channel.type === 5 || msg.channel.type === 0)){return;}
+        if(!(msg.channel.type === 0 || msg.channel.type === 5)){return;}
         const guild = msg.channel.guild;
-        if(!await this.checkGuildEnabled(guild.id)){return;}
-        const config = await this.Hyperion.managers.guild.getModuleConfig<RRConfig>(guild.id, this.name);
-        if(!(config.rr instanceof Map)){config.rr = new Map<string, ReactionRole>(Object.entries(config.rr ?? {}));}
-        if(config.rr.size === 0){return;}
-        const rrName = config.rrMessages.get(msg.id);
-        if(!rrName){return;}
-        const rr = config.rr.get(rrName);
+        const enabled = await this.checkGuildEnabled(guild.id);
+        if(!enabled){return;}
+        const config = await this.getConfig(guild.id);
+        if(config.rrMessages.size === 0){return;}
+        const rr = config.rrMessages.get(msg.id);
         if(!rr){return;}
-        if(!(rr.erMap instanceof Map)){rr.erMap = new Map<string, string>(Object.entries(rr.erMap ?? {}));}
-        const roleID = rr.erMap.get(emote.name);
-        if(!roleID){return;}
-        const role = guild.roles.get(roleID);
+        const rrData = config.rr.get(rr);
+        if(!rrData){return;}
+        const result = this.matchEmote([...rrData.erMap.keys()], emote);
+        if(!result){return;}
+        const role = rrData.erMap.get(result);
         if(!role){return;}
-        const member = guild.members.get(user) ?? await guild.getRESTMember(user).catch(() => undefined);
-        if(!member){return;}
-        if(member.bot){return;}
-        member.removeRole(roleID, "Hyperion Reaction Role").catch(err => this.Hyperion.logger.error("Hyperion", `Failed to add reaction role, err: ${err}`, "Reaction Roles"));
+        const usr = this.Hyperion.client.users.get(user) || await this.Hyperion.client.getRESTUser(user).catch(() => undefined);
+        if(usr && usr.bot){return;}
+        guild.removeMemberRole(user, role, "Hyperion Reaction Role").catch(err => console.warn(`Failed to give RR: ${err.message}`));
+    }
+
+    matchEmote(emotes: Array<string>, input: {id?: string; name: string}): string | undefined{
+        const parsed = emotes.map(em => this.parseEmote(em)).filter(em => em !== undefined);
+        for(const em of parsed){
+            if(typeof em === "string"){
+                if(input.name === em){return em;}
+            }else{
+                if(input.id && em?.id && em?.id === input?.id && em.name === input.name){return em.full;}
+            }
+        }
     }
 
     async addReactions(emotes: Array<string>, msg: Message): Promise<{failed: Array<string>; reason?: string}>{
+        emotes = emotes.map(em => {
+            return (em.startsWith("<") || em.endsWith(">")) ? em.slice(1, em.length -1) : em;
+        });
         for(const emote of emotes){
             try{
                 await msg.addReaction(emote);
@@ -74,7 +98,7 @@ class Reactionroles extends Module{
                 if(err.code === 30010){
                     const temp = emotes.indexOf(emote);
                     const failed = emotes.slice(temp);
-                    return {failed: failed, reason : "Max number of reactions reached"};
+                    return {failed: failed, reason: "Max number of reactions reached"};
                 }
                 if(err.code === 10014){
                     return {failed: [emote], reason: "Cannot use this emote"};
@@ -89,6 +113,91 @@ class Reactionroles extends Module{
         if(!channel.permissionsOf(this.Hyperion.client.user.id).has("addReactions")){return false;}
         if(!channel.permissionsOf(this.Hyperion.client.user.id).has("readMessageHistory")){return false;}
         return true;
+    }
+
+    async getConfig(guild: string){
+        return await this.Hyperion.managers.guild.getModuleConfig<RRConfig>(guild, this.name);
+    }
+
+    async createRR(guild: string, name: string){
+        const config = await this.getConfig(guild);
+        if(config.rr.has(name)){throw new Error("That reaction role already exists!");}
+        const isPro = await this.Hyperion.managers.guild.isPro(guild);
+        if(config.rr.size >= 3 && !isPro){throw new Error("Maximum number of free reaction roles reached");}
+        config.rr.set(name, new ReactionRole({}));
+        try{
+            await this.Hyperion.managers.guild.updateModuleConfig(guild, this.name, config);
+        }catch(err){
+            throw new Error(err.message);
+        }
+    }
+
+    async deleteRR(guild: string, name: string){
+        const config = await this.getConfig(guild);
+        if(!config.rr.has(name)){throw new Error("That reaction role doesnt exist!");}
+        config.rr.delete(name);
+        try{
+            await this.Hyperion.managers.guild.updateModuleConfig(guild, this.name, config);
+        }catch(err){
+            throw new Error(err.message);
+        }
+    }
+
+    async addRole(guild: string, rrName: string, emote: string, role: string): Promise<void>{
+        const config = await this.getConfig(guild);
+        const rr = config.rr.get(rrName);
+        if(!rr){throw new Error("That reaction role doesnt exist");}
+        if(rr.erMap.has(emote)){throw new Error("That emote is already used in this reaction role");}
+        if(rr.erMap.size >= 15){throw new Error("Maximum amount of roles reach for this reaction role");}
+        const isPro = await this.Hyperion.managers.guild.isPro(guild);
+        if(!isPro && rr.erMap.size >= 6 ){throw new Error("Maximum amount of free roles reached.");}
+        rr.erMap.set(emote, role);
+        config.rr.set(rrName, rr);
+        await this.Hyperion.managers.guild.updateModuleConfig(guild, this.name, config);
+    }
+
+    async delRole(guild: string, rrName: string, emote: string): Promise<void>{
+        const config = await this.getConfig(guild);
+        const rr = config.rr.get(rrName);
+        if(!rr){throw new Error("That reaction role doesnt exist");}
+        const toRemove = this.parseEmote(emote) ?? emote;
+        if(typeof toRemove === "string"){
+            let found = "";
+            for(const em of [...rr.erMap.keys()]){
+                const temp = this.parseEmote(em);
+                if(temp?.name === emote || temp?.id === emote){
+                    found = em;
+                    break;
+                }
+            }
+            if(found === ""){throw new Error("Could not find that emotes in this reaction role");}
+            rr.erMap.delete(found);
+            config.rr.set(rrName, rr);
+            await this.Hyperion.managers.guild.updateModuleConfig(guild, this.name, config);
+            return;
+        }else{
+            if(!rr.erMap.has(toRemove.full)){throw new Error("Could not find that emotes in this reaction role");}
+            rr.erMap.delete(toRemove.full);
+            config.rr.set(rrName, rr);
+            await this.Hyperion.managers.guild.updateModuleConfig(guild, this.name, config);
+            return;
+        }
+    }
+
+    isEmote(input: string,): boolean {
+        const EmoteRegex = new RegExp(/<(a)?:(\w+):(\d+)>/, "gmi");
+        const result = EmoteRegex.exec(input);
+        if(result){return true;}
+        return this.Hyperion.utils.hasUnicodeEmote(input);
+    }
+
+    parseEmote(input: string): {name: string; id?: string, full: string} | undefined {
+        const EmoteRegex = new RegExp(/<(a)?:(\w+):(\d+)>/, "gmi");
+        const result = EmoteRegex.exec(input);
+        if(result){
+            return {name: result[2], id: result[3], full: input};
+        }
+        if(this.Hyperion.utils.hasUnicodeEmote(input)){return {name: input, full: input};}
     }
 }
 export default Reactionroles;
