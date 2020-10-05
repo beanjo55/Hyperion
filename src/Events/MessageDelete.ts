84f81f1e949dbead6eba922039e444730453907d
beanjo55 <benschweitzer24@gmail.com>
2020-10-05 17:40:45 -0400

    typings

--- src/Events/MessageDelete.ts
+++ src/Events/MessageDelete.ts
@@ -1,5 +1,5 @@
 import {IHyperion} from "../types";
-import {Message} from "eris";
+import {GuildTextableChannel, Message} from "eris";
 const eventName = "messageDelete";
 class MessageDeleteHandler{
     name: string;
@@ -7,7 +7,7 @@ class MessageDeleteHandler{
         this.name = "messageDelete";
     }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    async handle(this: IHyperion, msg: Message | any): Promise<void>{
+    async handle(this: IHyperion, msg: Message | {id: string; channel: GuildTextableChannel}): Promise<void>{
         const subscribed = this.modules.filter(M => M.subscribedEvents.includes(eventName));
         subscribed.forEach(m => {
             m.messageDelete(this, msg);
