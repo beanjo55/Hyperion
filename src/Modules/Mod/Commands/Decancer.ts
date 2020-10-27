adb06daf3758e57c65848cab861cc4c357724a05
beanjo55 <benschweitzer24@gmail.com>
2020-10-27 00:14:02 -0400

    properly use can moderate check

--- src/Modules/Mod/Commands/Decancer.ts
+++ src/Modules/Mod/Commands/Decancer.ts
@@ -2,6 +2,7 @@ import {Command} from "../../../Core/Structures/Command";
 import {ICommandContext, IHyperion} from "../../../types";
 import {default as limax} from "limax";
 import {default as unorm} from "unorm";
+import {default as Mod} from "../Mod";
 
 class Decancer extends Command{
     constructor(){
@@ -16,14 +17,14 @@ class Decancer extends Command{
         });
     }
 
-    async execute(ctx: ICommandContext, Hyperion: IHyperion): Promise<string>{
+    async execute(ctx: ICommandContext<Mod>, Hyperion: IHyperion): Promise<string>{
         const bot = ctx.guild.members.get(Hyperion.client.user.id);
         if(!bot){return "Cache failure, could not find bot user!";}
         if(!bot.permission.has("manageNicknames")){return "I need permissions to manage nicknames to decancer someone's name.";}
         if(!ctx.args[0]){return "Please specify a user.";}
         const target = Hyperion.utils.strictResolver(ctx.args[0], ctx.guild.members);
         if(!target){return "Invalid user provided, try their user ID or mention.";}
-        if(!ctx.module.canModerate(Hyperion, target, bot, ctx.guild)){return "I cant manage that user.";}
+        if(!(await ctx.module.canModerate(target, bot, ctx.guild))){return "I cant manage that user.";}
         const text = target.nick ?? target.username;
         const cleaned = this.decancer(text);
         if(cleaned === text){return "The decancered result was the same.";}
